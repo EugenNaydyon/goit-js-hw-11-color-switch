1615aefc89855a36818901f14331e1b346f40568
EugenNaydyon <eugennaydyon@gmail.com>
2021-08-05 02:08:08 +0300

    working version

--- src/index.js
+++ src/index.js
@@ -6,3 +6,31 @@ const colors = [
   "#009688",
   "#795548",
 ];
+
+const refs = {
+  bodyElement: document.querySelector("#body"),
+  buttonStart: document.querySelector("#start"),
+  buttonStop: document.querySelector("#stop"),
+};
+
+let switchId = null;
+
+function colorSwitch() {
+  refs.buttonStart.setAttribute("disabled", true);
+  switchId = setInterval(() => {
+    refs.bodyElement.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, 5)];
+  }, 1000);
+}
+
+function stopColorSwitch() {
+  refs.buttonStart.removeAttribute("disabled");
+  clearInterval(switchId);
+}
+
+refs.buttonStart.addEventListener("click", colorSwitch);
+refs.buttonStop.addEventListener("click", stopColorSwitch);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
